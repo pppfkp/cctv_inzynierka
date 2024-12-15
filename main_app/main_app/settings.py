73c9b9a359ef9d172bc02c57eea338c73e945f8c
Filip Lepa <pppfkp@outlook.com>
2024-12-15 12:24:22 +0100

    docker fixes

--- main_app/main_app/settings.py
+++ main_app/main_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'main_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGVECTOR_DB_NAME'),
+        'USER': os.getenv('PGVECTOR_DB_USER'),
+        'PASSWORD': os.getenv('PGVECTOR_DB_PASSWORD'),
+        'HOST': os.getenv('PGVECTOR_DB_HOST'),  
+        'PORT': os.getenv('PGVECTOR_DB_PORT'),        
     }
 }
 
