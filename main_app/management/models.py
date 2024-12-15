d8692989440938d9d6d772ef288a9490bc9b8796
Filip Lepa <pppfkp@outlook.com>
2024-12-15 14:08:40 +0100

    added management models

--- main_app/management/models.py
+++ main_app/management/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from pgvector.django import VectorField
 
 # Create your models here.
 class TrackingSubject(models.Model):
@@ -12,4 +13,37 @@ class TrackingSubject(models.Model):
 def create_tracking_subjects(sender, instance, created, **kwargs):
     if created:
         TrackingSubject.objects.create(user=instance)
-        
+
+class Camera(models.Model):
+    link = models.CharField(max_length=500, unique=True)
+    name = models.CharField(max_length=200, unique=True)
+    enabled = models.BooleanField(default=True)
+    transformation_matrix = VectorField(dimensions=9, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}: {self.link}"
+    
+class Setting(models.Model):
+    setting_key = models.CharField(max_length=100, unique=True)
+    value = models.TextField()
+    description = models.TextField(blank=True, null=True)
+    TYPE_CHOICES = [
+        ('str', 'String'),
+        ('int', 'Integer'),
+        ('bool', 'Boolean'),
+        ('float', 'Float'),
+    ]
+    data_type = models.CharField(max_length=10, choices=TYPE_CHOICES, default='str')
+
+    def __str__(self):
+        return f"{self.key}: {self.value}"
+    
+class CalibrationPoint(models.Model):
+    camera = models.ForeignKey(Camera, on_delete=models.CASCADE, related_name='calibration_points')
+    canvas_x = models.FloatField()
+    canvas_y = models.FloatField()
+    camera_x = models.FloatField()
+    camera_y = models.FloatField()
+
+    def __str__(self):
+        return f"Calibration for {self.camera.name} - Canvas ({self.canvas_x}, {self.canvas_y}), Camera ({self.camera_x}, {self.camera_y})"    
--- main_app/management/utils.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-
-@receiver(post_save, sender=User)
-def create_favorites(sender, instance, created, **kwargs):
-    if created:
-        Favorites.objects.create(user=instance)
