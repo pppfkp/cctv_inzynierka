61898cc1ca360a393468b513cd5996a726d4018a
Filip Lepa <pppfkp@outlook.com>
2024-12-15 11:51:54 +0100

    cleaned

--- main_app/main_app/urls.py
+++ main_app/main_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cctv_django_project project.
+URL configuration for main_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cctv_admin/', include("cctv_admin.urls"))
 ]
