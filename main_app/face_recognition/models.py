dcbf8238c99f508351304151ad4bef42ced09184
Filip Lepa <pppfkp@outlook.com>
2024-12-24 11:08:56 +0100

    Added a photo field to face_embedding model

--- main_app/face_recognition/models.py
+++ main_app/face_recognition/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class FaceEmbedding(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='face_embeddings')
     embedding = VectorField(dimensions=512, null=False, blank=False)
+    photo = models.ImageField(upload_to='face_photos/', null=False, blank=False)
 
     def __str__(self):
         return f"FaceEmbedding for {self.user}"
--- main_app/main_app/urls.py
+++ main_app/main_app/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from face_recognition.views import extract_embedding_view, FindClosestEmbeddingView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('face_recognition/extract_embedding/', extract_embedding_view, name='extract-embedding-view'),
     path('face_recognition/api/recognize/',FindClosestEmbeddingView.as_view())
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
