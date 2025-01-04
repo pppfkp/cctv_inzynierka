735870db832115d2cf35a166e528ecb1224a2e17
Filip Lepa <pppfkp@outlook.com>
2025-01-04 12:11:14 +0100

    Moved camera link to .env

--- detection/test.py
+++ detection/test.py
@@ -1,12 +1,15 @@
 from flask import Flask, Response
 import cv2
 from utils import open_camera, generate_frames
+import os
+
+camera_url = os.getenv("CAMERA_URL")
 
 # Initialize Flask app
 app = Flask(__name__)
 
 # Open the camera feed
-cap = open_camera("http://192.168.0.156:5333/video")
+cap = open_camera(camera_url)
 
 @app.route('/video_feed')
 def video_feed():
