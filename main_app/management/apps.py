34c6233c37f95575d88269cc32a402462cd4ac64
Filip Lepa <pppfkp@outlook.com>
2025-01-07 14:03:20 +0100

    Implemented starting containers at app startup

--- main_app/management/apps.py
+++ main_app/management/apps.py
@@ -3,17 +3,14 @@ import docker
 import logging
 
 
+
 class ManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'management'
     def ready(self):
         try:
-            # Initialize Docker client
-            client = docker.DockerClient(base_url='tcp://host.docker.internal:2375')
-            
-            # Example: List all containers
-            containers = client.containers.list()
-            for container in containers:
-                logging.info(f"Container name: {container.name}")
+            from .views import restart_containers
+
+            restart_containers(None)
         except docker.errors.DockerException as e:
             logging.error(f"Error connecting to Docker: {e}")
