3994847ea4f1ad630eb22f5fd9178255c2f879d2
Filip Lepa <pppfkp@outlook.com>
2025-01-11 10:44:29 +0100

    Removed unused camera model fields

--- main_app/management/models.py
+++ main_app/management/models.py
@@ -20,8 +20,6 @@ def create_tracking_subjects(sender, instance, created, **kwargs):
 class Camera(models.Model):
     link = models.CharField(max_length=500, unique=True)
     name = models.CharField(max_length=200, unique=True)
-    enabled = models.BooleanField(default=True)
-    transformation_matrix = VectorField(dimensions=9, null=True, blank=True)
 
     def __str__(self):
         return f"{self.name}: {self.link}"
